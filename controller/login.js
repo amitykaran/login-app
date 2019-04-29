6208f573be78113c0874d735ee6b54bccd7d6825
Amit Yadav <amitykaran74@gmail.com>
2019-04-29 23:53:35 +0530

    signup using hashed password

--- controller/login.js
+++ controller/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const _  = require('lodash');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 const userModel = require('../models/user');
@@ -10,20 +11,31 @@ router.post('/login', (req, res) => {
         user = req.body.user;
     }
     if(!_.isEmpty(user)){
-        userModel.find({})
-        .select({email: user.email})
+        userModel.find({email: user.email})
         .exec((err, obj) => {
             if(err){
                 console.log(err);
                 res.send(`Error ${err}`);
             } else{
                 console.log('User found', obj);
-                res.json({
-                    status: 'success',
-                    user: obj
-                })
+                if(bcrypt.compare(user.password, obj.password)){
+                    console.log('Congratulation! you are successfully loggedIn');
+                    res.json({
+                        status: 'success',
+                        result : 'You are logged in successfully'
+                    })
+                } else {
+                    console.log('Your password is incorrect, Please try again');
+                    res.status(403).send();
+                }
+                // res.json({
+                //     status: 'success',
+                //     user: obj
+                // })
             }
         })
+    } else {
+        res.send('Please enter a valid user');
     }
 });
 
--- controller/user.js
+++ controller/user.js
@@ -1,32 +1,54 @@
 const express = require('express');
 const _ = require('lodash');
+const bcrypt  = require('bcrypt');
 
 
 const router = express.Router() ;
 const userModel = require('../models/user');
-
+var BCRYPT_SALT_ROUNDS = 12;
 
 router.post('/add-user', (req, res) => {
     let user = {};
     if(req.body.user) {
         user = req.body.user;
+
     }
     console.log('req received is ', req.body);
     if(!_.isEmpty(user)) {
-        console.log(`${user.name} received`);
-        let newUser = new userModel(user);
-        newUser.save((err, object) => {
+        userModel.find({email: user.email})
+        .exec((err, obj) => {
             if(err){
-                console.log(err);
-            } else {
-                console.log(`${object} saved successfully`);
-            }
+                res.send('Error in finding user');
+            } else{
+                if(!_.isEmpty(obj)){
+                    console.log('This email is already registered!');
+                res.send('This email is alredy registered. Please SignUp with another ID');
+   
+                } else{
+                    bcrypt.hash(user.password, BCRYPT_SALT_ROUNDS).then((hashedPassword) => {
+                        user.password = hashedPassword;
+                        let newUser = new userModel(user);
+                        newUser.save((err, object) => {
+                            if(err){
+                                console.log(err);
+                            } else {
+                                console.log(`${object} saved successfully`);
+                            }
+                        });
+                    }).then(() => {
+                        res.send('User created successfully');
+                    }).catch((err) => {
+                        console.log('Error in creating user. Please try again', err);
+                        res.send(err);
+                    });
+                    console.log(`${user.name} received`);
+                }
+                         }
         });
-
-    } else {
+    }  else {
         console.log('User NOT found');
+        res.send('Please enter a valid user');
     }
-    res.send('this is response from add-user');
 });
 
 router.get('/user-list', (req, res) => {
